f78c9d4ea576792ed29060910ffa1207f94cc284
Mike Kelly <mike.forge@gmail.com>
2022-03-31 20:30:17 -0400

    setting up router for page switches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Header, Footer } from './components/common';
 import './style.css';
+
 
 
 const App = () => {
     return (
     <div>
         <Header />
+
+        <Router>
+            <Switch>
+                <Route path='/about'>
+                    About Me
+                </Route>
+                <Route path='/portfolio'>
+                    Portfolio
+                </Route>
+                <Route path='/contact'>
+                    Contact Me
+                </Route>
+                <Route path='/'>
+                    Home
+                </Route>
+            </Switch>
+        </Router>
+
         <Footer />
         </div>
 
